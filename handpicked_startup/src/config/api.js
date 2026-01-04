cdc78e77f4a42eaa899ca7994a72f23e814496a2
Suresh Roshanlal <suresh.roshanlal@gmail.com>
2026-01-04 17:35:27 +0530

    login page upgrade

--- handpicked_startup/src/config/api.js
+++ handpicked_startup/src/config/api.js
@@ -1,5 +1,5 @@
 // export const API_BASE_URL = 'https://admin-api.geniecoupon.com';
 // export const API_BASE_URL = 'http://localhost:5000';
 
-export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.PUBLIC_API_BASE_URL;
 console.log("ENV CHECK", import.meta.env);
